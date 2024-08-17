541a2cd874b5089cac676d2403c5583e6df32613
Terry0107 <1231100992@student.mmu.edu.my>
2024-08-18 02:06:56 +0800

    flask

--- main.py
+++ main.py
@@ -1,6 +1,18 @@
-from website import create_web
+from flask import Flask, render_template
 
-web = create_web()
+main = Flask(__name__)
 
-if __name__ == '__main__':
-    web.run(debug=True)
+@main.route('/')
+def Index():
+    return render_template('index.html')
+
+@main.route('/signup')
+def SignUp():
+    return render_template('signup.html')
+
+@main.route('/login')
+def Login():
+    return render_template('signin.html')
+
+if __name__ == "__main__":
+    main.run(debug=True)
