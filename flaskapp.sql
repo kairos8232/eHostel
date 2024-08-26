41bb5b4e07b9188e45a5a56d0629725f645881b2
Yun Loong <wksl.yl36@gmail.com>
2024-08-26 10:19:08 +0800

    RoomPageAdded

---
+++ flaskapp.sql
@@ -0,0 +1,53 @@
+-- --------------------------------------------------------
+-- Host:                         127.0.0.1
+-- Server version:               8.0.39 - MySQL Community Server - GPL
+-- Server OS:                    Win64
+-- HeidiSQL Version:             12.8.0.6908
+-- --------------------------------------------------------
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET NAMES utf8 */;
+/*!50503 SET NAMES utf8mb4 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+
+-- Dumping database structure for flaskapp
+CREATE DATABASE IF NOT EXISTS `flaskapp` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_520_ci */ /*!80016 DEFAULT ENCRYPTION='N' */;
+USE `flaskapp`;
+
+-- Dumping structure for table flaskapp.rooms
+CREATE TABLE IF NOT EXISTS `rooms` (
+  `number` int DEFAULT NULL,
+  `category` varchar(50) COLLATE utf8mb4_unicode_520_ci DEFAULT NULL,
+  `capacity` int DEFAULT NULL,
+  `status` varchar(50) COLLATE utf8mb4_unicode_520_ci DEFAULT NULL,
+  `cost` int DEFAULT NULL,
+  `chosen_by` varchar(50) COLLATE utf8mb4_unicode_520_ci DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_520_ci;
+
+-- Dumping data for table flaskapp.rooms: ~2 rows (approximately)
+INSERT INTO `rooms` (`number`, `category`, `capacity`, `status`, `cost`, `chosen_by`) VALUES
+	(123, 'Single', 1, 'Available', 500, NULL),
+	(101, 'Double', 2, 'Occupied', 400, NULL);
+
+-- Dumping structure for table flaskapp.users
+CREATE TABLE IF NOT EXISTS `users` (
+  `sid` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_520_ci DEFAULT NULL,
+  `email` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_520_ci DEFAULT NULL,
+  `gender` varchar(50) COLLATE utf8mb4_unicode_520_ci DEFAULT NULL,
+  `password` varchar(50) COLLATE utf8mb4_unicode_520_ci DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_520_ci;
+
+-- Dumping data for table flaskapp.users: ~1 rows (approximately)
+INSERT INTO `users` (`sid`, `email`, `gender`, `password`) VALUES
+	('1231102119', '1234@gmail.com', 'male', '123');
+
+/*!40103 SET TIME_ZONE=IFNULL(@OLD_TIME_ZONE, 'system') */;
+/*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
+/*!40014 SET FOREIGN_KEY_CHECKS=IFNULL(@OLD_FOREIGN_KEY_CHECKS, 1) */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40111 SET SQL_NOTES=IFNULL(@OLD_SQL_NOTES, 1) */;
---
+++ main.py
@@ -0,0 +1,114 @@
+from flask import Flask, render_template, request, redirect, url_for, session
+from flask_mysqldb import MySQL
+import MySQLdb.cursors
+import re
+import yaml
+
+main = Flask(__name__)
+
+def create_main():
+    main =  Flask(__name__)
+    main.config['SECRET_KEY'] = 'Hostel management secret key'
+    return main
+
+db = yaml.safe_load(open('db.yaml'))
+main.config['MYSQL_HOST'] = db['mysql_host']
+main.config['MYSQL_USER'] = db['mysql_user']
+main.config['MYSQL_PASSWORD'] = db['mysql_password']
+main.config['MYSQL_DB'] = db['mysql_db']
+main.secret_key = 'terrychin'
+
+mysql = MySQL(main)
+
+
+@main.route('/')
+def Index():
+    return render_template('index.html')
+
+@main.route("/home")
+def home():
+    return render_template('home.html', name = session['sid'])
+
+@main.route('/signup', methods = ['POST', 'GET'])
+def SignUp():
+    if request.method == 'POST':
+        userDetails = request.form
+        sid = userDetails['sid']
+        email = userDetails['email']
+        gender = userDetails['gender']
+        password = userDetails['password']
+        
+        cur = mysql.connection.cursor()
+        cur.execute("INSERT INTO users(sid, email, gender, password) VALUES(%s, %s, %s, %s)", (sid, email, gender, password))
+        mysql.connection.commit()
+        cur.close()
+        return redirect(url_for("home"))
+    return render_template('signup.html')
+
+@main.route('/login' , methods = ['POST' , 'GET'])
+def Login():
+    if request.method == 'POST':
+        userDetails = request.form
+        sid = userDetails['sid']
+        password = userDetails['password']
+        cur = mysql.connection.cursor()
+        cur.execute('SELECT * FROM users WHERE sid=%s AND password=%s', (sid, password))
+        record = cur.fetchone()
+        if record:
+            session['loggedin']= True
+            session['sid']= record[1]
+            session['name'] = record[0]
+            return redirect(url_for('home'))
+        else:
+            msg='Incorrect username/password. Try again!'
+            return render_template('index.html', msg = msg)
+    
+    return render_template('signin.html')
+
+@main.route('/room')
+def room_list():
+    # Use the MySQL connection from flask_mysqldb
+    cur = mysql.connection.cursor(MySQLdb.cursors.DictCursor)
+
+    # Fetch room data from the database
+    cur.execute("SELECT number, category, capacity, status, cost FROM rooms")
+    rooms = cur.fetchall()
+
+    # Close database connection
+    cur.close()
+
+    # Render the HTML template with the room data
+    return render_template('room.html', rooms=rooms)
+
+@main.route('/choose-room', methods=['POST'])
+def choose_room():
+    if 'email' not in session:
+        return redirect(url_for('login'))
+
+    room_number = request.form['room_number']
+    user_email = session['sid']
+
+    cursor = mysql.connection.cursor(MySQLdb.cursors.DictCursor)
+    cursor.execute("SELECT chosen_by FROM rooms WHERE number = %s", (room_number,))
+    room = cursor.fetchone()
+
+    if room and room['chosen_by'] is None:
+        cursor.execute("UPDATE rooms SET chosen_by = %s WHERE number = %s", (user_email, room_number))
+        mysql.connection.commit()
+    cursor.close()
+
+    print("Room chosen:", room_number)
+
+    return redirect(url_for('room_list'))
+
+@main.route('/test-submit', methods=['POST'])
+def test_submit():
+    # This is just for testing purposes
+    room_number = request.form.get('room_number')
+    return f"Room number {room_number} received!"
+
+
+
+
+if __name__ == "__main__":
+    main.run(debug=True) 
